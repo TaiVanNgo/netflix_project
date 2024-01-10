07d025af32bfae967c33f04620af87213ed49bd8
TaiVanNgo <vantaingo.056@gmail.com>
2024-01-10 11:45:57 +0700

    THE LAST COMMIT (MAYBE) :(

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,2 +1,12 @@
-import { applyMiddleware } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWidthDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const middleware = [thunk];
+const store = createStore(
+    rootReducer,
+    composeWidthDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
